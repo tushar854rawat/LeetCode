44c489ed57294ee80851f416689b561ffa9b2ee8
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-08-17 11:35:31 +0530

    Time: 2 ms (98.54%), Space: 10.1 MB (10.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<int,int>m;
+
+        for(auto x : s){
+            m[x]++;
+        }
+        for(auto x : t){
+            m[x]--;
+        }
+
+        for(auto x : m){
+            if(x.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
