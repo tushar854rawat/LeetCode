8cbb4395704c50a3ce59c0eca13d1f766bcd138e
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-08-23 11:25:44 +0530

    Time: 0 ms (100%), Space: 9.5 MB (46.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,15 +11,26 @@ public:
         int n = nums.size();
         if(n==0)return 0;
         if(n==1)return nums[0];
-        vector<int>dp(n,0);
+        // vector<int>dp(n,0);
         // return f(nums,n-1,dp);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0],nums[1]);
+        // for(int i=2;i<n;i++){
+        //     int inc = nums[i]+dp[i-2];
+        //     int exe = 0+dp[i-1];
+        //     dp[i] = max(inc,exe);
+        // }
+        // return dp[n-1];
+        int prev0 = nums[0];
+        int prev1 = max(nums[0],nums[1]);
+        int sum = 0;
         for(int i=2;i<n;i++){
-            int inc = nums[i]+dp[i-2];
-            int exe = 0+dp[i-1];
-            dp[i] = max(inc,exe);
+            int incl = nums[i]+prev0;
+            int exe = 0+prev1;
+            sum = max(incl,exe);
+            prev0=prev1;
+            prev1=sum;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
