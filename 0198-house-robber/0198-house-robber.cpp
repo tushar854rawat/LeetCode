df2d3083540f3ba4feca602cde994b46c8d084ab
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-08-23 09:21:53 +0530

    Time: 2 ms (39.54%), Space: 9.5 MB (46.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,7 +9,17 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,-1);
-        return f(nums,n-1,dp);
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        vector<int>dp(n,0);
+        // return f(nums,n-1,dp);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int inc = nums[i]+dp[i-2];
+            int exe = 0+dp[i-1];
+            dp[i] = max(inc,exe);
+        }
+        return dp[n-1];
     }
 };
