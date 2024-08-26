8a633821ca864ca0318d323db4322d28733596d3
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-08-26 12:04:20 +0530

    Time: 6 ms (90.1%), Space: 12.9 MB (90.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -15,19 +15,36 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        // return f(0,-1,nums,dp);
+        if(n==0){
+            return 0;
+        }
+        vector<int>ans;
+        ans.push_back(nums[0]);
 
-        for(int curr=n-1;curr>=0;curr--){
-            for(int prev=curr;prev>=-1;prev--){
-                int incl = 0;
-                if(prev==-1 || nums[curr]>nums[prev]){
-                    incl = 1+dp[curr+1][curr+1];
-                }
-                int exe = 0+dp[curr+1][prev+1];
-                dp[curr][prev+1] = max(incl,exe);
+        for(int i=1;i<n;i++){
+            if(nums[i]>ans.back()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                int ind = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[ind] = nums[i];
             }
         }
-        return dp[0][0];
+        return ans.size();
+        // int n = nums.size();
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // return f(0,-1,nums,dp);
+
+        // for(int curr=n-1;curr>=0;curr--){
+        //     for(int prev=curr;prev>=-1;prev--){
+        //         int incl = 0;
+        //         if(prev==-1 || nums[curr]>nums[prev]){
+        //             incl = 1+dp[curr+1][curr+1];
+        //         }
+        //         int exe = 0+dp[curr+1][prev+1];
+        //         dp[curr][prev+1] = max(incl,exe);
+        //     }
+        // }
+        // return dp[0][0];
     }
 };
