e9b6b74f6861a3e9d58a224cc8769198ad51198a
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-08-28 19:50:01 +0530

    Time: 27 ms (50.13%), Space: 25.9 MB (59.36%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         if(i>=a.size()) return 0;
         if(j>=b.size()) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        
+
         int ans = 0;
         if(a[i]==b[j]){
             ans = 1+f(a,b,i+1,j+1,dp);
@@ -16,7 +16,20 @@ public:
     }
     int longestCommonSubsequence(string a, string b) {
         int n = a.length(),m = b.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(a,b,0,0,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // return f(a,b,0,0,dp);
+        int ans = 0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(a[i]==b[j]){
+                    ans = 1+dp[i+1][j+1];
+                }
+                else{
+                    ans = max(dp[i+1][j],dp[i][j+1]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
     }
 };
