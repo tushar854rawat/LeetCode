dda97effade700360c340ceae4d11f5f29982861
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-08-29 15:43:08 +0530

    Time: 1830 ms (5.16%), Space: 91.9 MB (89.77%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -2,15 +2,24 @@ class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(n);
-        vector<int>sorted;
+        vector<int> ans(n, 0);
+        vector<int> sortedList;
 
-        for(int i=n-1;i>=0;i--){
-            int ind = lower_bound(sorted.begin(),sorted.end(),nums[i])-sorted.begin();
-            ans[i]=ind;
-
-            sorted.insert(sorted.begin() + ind, nums[i]);
+        for (int i = n - 1; i >= 0; --i) {
+            int left = 0, right = sortedList.size();
+            // Perform binary search to find the first index where nums[i] should go
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (sortedList[mid] < nums[i]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            ans[i] = left; // number of elements smaller than nums[i]
+            sortedList.insert(sortedList.begin() + left, nums[i]);
         }
+
         return ans;
     }
 };
