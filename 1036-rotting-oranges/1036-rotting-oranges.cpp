b5e90e604bf1a1f76edc0016e64f7a1a9d8a3096
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-11-28 11:44:41 +0530

    Time: 0 ms (100%), Space: 17.2 MB (11.06%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,36 +3,36 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int vis[n][m];
-        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
-                    vis[i][j]=2;
-                }
-                else{
-                    vis[i][j]=0;
+                    vis[i][j] = 2;
                 }
             }
         }
 
-        int tm = 0;
-        int drow[] = {-1,0,+1,0};
-        int dcol[] = {0,+1,0,-1};
+        int total = 0;
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
-            tm = max(tm,t);
+            total = max(total,t);
             q.pop();
+
             for(int i=0;i<4;i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
+                int nr = r + drow[i];
+                int nc = c + dcol[i];
+
+                if(nr >= 0 && nr < n && nc >=0 && nc < m && vis[nr][nc]==0 && grid[nr][nc]==1){
+                    q.push({{nr,nc},t+1});
+                    vis[nr][nc] = 2;
                 }
             }
         }
@@ -44,6 +44,6 @@ public:
                 }
             }
         }
-        return tm;
+        return total;
     }
 };
