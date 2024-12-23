e03f003153deb950c0f8a1ba799a52355b9374fd
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-12-23 10:03:37 +0530

    Time: 15 ms (92.68%), Space: 17.8 MB (59.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    int f(int ind,vector<int>&arr,int amount,vector<vector<int>>&dp){
-        if(ind==0){
-           if(amount%arr[ind]==0) return amount/arr[0];
-           return 1e9;
-        }
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int nottake = 0 + f(ind-1,arr,amount,dp);
-        int take = INT_MAX;
-        if(arr[ind]<=amount){
-            take = 1 + f(ind,arr,amount-arr[ind],dp);
-        }
-        return dp[ind][amount] = min(take,nottake);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int a = f(n-1,coins,amount,dp);
-        if(a >= 1e9){
-            return -1;
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0] = 0;
+        for(auto coin : coins){
+            for(int i = coin;i <= amount; i++){
+                if(dp[i-coin] != INT_MAX){
+                    dp[i] = min(dp[i],1 + dp[i-coin]);
+                }
+            }
         }
-        return a;
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
