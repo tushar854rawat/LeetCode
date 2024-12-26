baa9ec73b9dcf70d35d845347ae4641079e50b59
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2024-12-26 16:39:50 +0530

    Time: 1309 ms (7.04%), Space: 295.6 MB (7.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,28 +3,29 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         set<vector<int>>st;
-        vector<vector<int>>ans;
-        sort(begin(nums),end(nums));
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++){
-                int low = i+1,high = n-1;
-                while(low<high){
-                if(nums[i]+nums[low]+nums[high]<0){
+            int low = i + 1,high = n - 1;
+            while(low<high){
+                if(nums[i] + nums[low] + nums[high]<0){
                     low++;
                 }
-                else if(nums[i]+nums[low]+nums[high]>0){
+                else if(nums[i] + nums[low] + nums[high]>0){
                     high--;
                 }
                 else{
                     st.insert({nums[i],nums[low],nums[high]});
-                    low++;
                     high--;
+                    low++;
                 }
             }
         }
-        
+
         for(auto x : st){
             ans.push_back(x);
         }
+
         return ans;
     }
 };
