b587692d561a54394ba3e63ec79917433fc14d58
tushar rawat <96645808+tushar854rawat@users.noreply.github.com>
2025-01-27 10:44:26 +0530

    Time: 45 ms (36.02%), Space: 49.5 MB (24.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,INT_MAX);
-        dp[0] = 0;
-        for(auto coin : coins){
-            for(int i = coin;i <= amount; i++){
-                if(dp[i-coin] != INT_MAX){
-                    dp[i] = min(dp[i],1 + dp[i-coin]);
-                }
+    int f(int ind,int tar,vector<int>& nums,vector<vector<int>>& dp){
+        if(ind==0){
+            if(tar%nums[ind]==0) return tar/nums[0];
+            return INT_MAX;
+        }
+        if(dp[ind][tar]!=-1) return dp[ind][tar];
+
+        int exe = 0 + f(ind-1,tar,nums,dp);
+        int incl = INT_MAX;
+        if(nums[ind]<=tar){
+            int a = f(ind,tar-nums[ind],nums,dp);
+            if(a!=INT_MAX){
+                incl = 1 + a;
             }
         }
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
+        return dp[ind][tar] = min(exe,incl);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = f(n-1,amount,coins,dp);
+        if(ans>=INT_MAX) return -1;
+        return ans;
     }
 };
